32a4da99b99288806432130aade1f259325c8cdb
Ning <Ning@hndream.top>
2018-11-15 18:47:36 +0800

    抽取项目的配置到单独的文件

---
+++ config.py
@@ -0,0 +1,27 @@
+from redis import StrictRedis
+
+
+class Config(object):
+    """项目的配置"""
+    DEBUG = True
+
+    SECRET_KEY ="Qki318sldkQy6nIjCrTXv+d1N5KCrtgAuab2SurD7INeMByADiIk0hrjGMtNZlE7"
+
+    #为Mysql添加配置
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Redis的配置
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+    # Session保存配置
+    SESSION_TYPE = "redis"
+    # 开启session签名
+    SESSION_USE_SIGNER = True
+    # 指定Session保存的redis
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
+    # 设置需要过期
+    SESSION_PERMANENT = False
+    # 设置过期时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
--- manage.py
+++ manage.py
@@ -1,5 +1,4 @@
 from flask.ext.wtf import CSRFProtect
-from flask_migrate import MigrateCommand, Migrate
 from flask_script import Manager
 from redis import StrictRedis
 from flask import Flask, session
@@ -7,32 +6,10 @@ from flask_sqlalchemy import SQLAlchemy
 # 可以用来制定session的位置
 from flask_session import Session
 from flask_migrate import Migrate,MigrateCommand
+from config import Config
 
 
-class Config(object):
-    """项目的配置"""
-    DEBUG = True
 
-    SECRET_KEY ="Qki318sldkQy6nIjCrTXv+d1N5KCrtgAuab2SurD7INeMByADiIk0hrjGMtNZlE7"
-
-    #为Mysql添加配置
-    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # Redis的配置
-    REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = 6379
-
-    # Session保存配置
-    SESSION_TYPE = "redis"
-    # 开启session签名
-    SESSION_USE_SIGNER = True
-    # 指定Session保存的redis
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
-    # 设置需要过期
-    SESSION_PERMANENT = False
-    # 设置过期时间
-    PERMANENT_SESSION_LIFETIME = 86400 * 2
 
 
 
@@ -65,7 +42,7 @@ manager.add_command('db',MigrateCommand)
 @app.route('/')
 def index():
     session["name"] = "itheima"
-    return 'index2222'
+    return 'index'
 
 
 if __name__ == '__main__':
