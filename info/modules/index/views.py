eef78df8ff529ee08a810413d2cc533527f7f3ec
Ning <Ning@hndream.top>
2018-11-17 18:40:32 +0800

    加载favicon.ico图标

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,10 +1,16 @@
+from flask import render_template, current_app
+
 from info import redis_store
 from . import index_blu
 
 
 @index_blu.route('/')
 def index():
-    # 向redis中保存一个值 name ning
+    return render_template('news/index.html')
 
-    redis_store.set("name","ning")
-    return 'index'
+
+# 再打开网页的时候,浏览器会默认去请求根路径+favicon.ico网站标签的小图标
+# send_static_file 是flask 去查找指定的静态文件所调用的方法
+@index_blu.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')
